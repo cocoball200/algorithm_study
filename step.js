e20f5d93bf7c832c9818d6ffd0a0709b6beabe63
mia <cocoball200@gmail.com>
2021-03-17 22:49:52 +0900

    계단찍기 재귀함수로 구현

--- step.js
+++ step.js
@@ -1,14 +1,27 @@
-function steps(n) {
+function step1(n) {
   for (let row = 0; row < n; row++) {
-    let stair = '';
+    let stair = "";
 
     for (let column = 0; column < n; column++) {
       if (column <= row) {
-        stair += '#';
+        stair += "#";
       } else {
-        stair += ' ';
+        stair += " ";
       }
     }
     console.log(stair);
   }
 }
+
+function step2(n, row = 0, stair = "") {
+  if (n === row) {
+    return;
+  }
+  if (n === stair.length) {
+    console.log(stair);
+    return step2(n, row + 1);
+  }
+  const add = stair.length >= row ? "#" : "";
+  step2(n, row, stair);
+}
+steps(5);
